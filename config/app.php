86f31e6be6e648b4299eeb84d2a0aca0545796cd
kesixin <462369233@qq.com>
2017-12-11 23:12:38 +0800

    [12-11]

--- config/app.php
+++ config/app.php
@@ -156,6 +156,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\Debugbar\ServiceProvider::class,
+
     ],
 
     /*
