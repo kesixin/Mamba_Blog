1000eb8e2c290732558b5694f853299db18bc8e9
kesixin <462369233@qq.com>
2018-07-25 23:38:22 +0800

    add 404.blade.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+        //return parent::render($request, $e);
+        return response()->view('errors.404');
     }
 }
